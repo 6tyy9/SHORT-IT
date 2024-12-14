3d4b661aeca74a4d8ce6744365cea2f790764b02
Shivanshu <rawat.shivanshu007@gmail.com>
2024-12-14 15:41:42 +0530

    updated landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
    <nav className='py-4 flex justify-between items-center'>
     <Link to='/'>
-    <img src='/logo.jpeg' className='h-16' alt='Short-It logo'></img>
+    <img src='/logoDark.png' className='h-16' alt='Short-It logo'></img>
     </Link>
      <div>
       {!user ?
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
+import { Form } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='flex flex-col items-center'>
+      <h2 className='my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold'>
+        The Only URL Shortener <br/>You'll Ever Need!🤌 
+      </h2>
+      <form className='sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2'>
+        <Input/>
+      <Button>Shorten!</Button>
+      </form>
+      <img src='/banner.jpeg' alt='Banner' className='w-full my-11 md:px-11' />
+    </div>
   )
 }
 
