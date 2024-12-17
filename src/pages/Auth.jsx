6d2cfcd1960e5768e2c99f6cefb50f557d4d228b
Shivanshu <rawat.shivanshu007@gmail.com>
2024-12-17 15:43:04 +0530

    Inducing login/signup functionality.

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Auth = () => {
+  const [searchParams]= useSearchParams()
   return (
-    <div>Auth</div>
+    <div className='mt-36 flex flex-col items-center gap-10'>
+      <h1 className='text-xl font-extrabold'>
+        {searchParams.get("createNew")?"Hold up! Lets login first...":"Login / Signup"}
+      </h1>
+    </div>
   )
 }
 
